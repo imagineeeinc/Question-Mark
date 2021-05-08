cf21c47c1b9efbd8de413ec3de4528c8610d4d0b
Imagineee <imagineeelego@gmail.com>
2021-05-08 11:17:25 +0100

    added search

--- scripts/require.js
+++ scripts/require.js
@@ -7,9 +7,33 @@ win = remote.getCurrentWindow()
 const contents = win.webContents
 var ipc = require('electron').ipcRenderer
 
-const fs = require('fs');
+const fs = require('fs')
+const path = require('path')
 const fetch = require('node-fetch')
 
+document.body.onclick = function() {
+    console.log(fromDir("./", 'index.html'))
+}
+function fromDir(startPath,filter){
+    var list = []
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter); //recurse
+        }
+        else if (filename.indexOf(filter)>=0) {
+            list.push(filename)
+        };
+    };
+    return list
+}
 /*
 var search = "imagineeeinc"
 fetch(`https://www.google.com/search?q=${search}`)
